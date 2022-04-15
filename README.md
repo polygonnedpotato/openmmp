629ca119d43dcd84bc3c4c1fcaa008cc23f2e604
elburg <whotookelburg@gmail.com>
2022-04-15 14:19:48 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Multimedia Web Player
+
+## TODO
+### Integrations
+- [ ] [Echo3 Framwork](https://github.com/echo3/echo3 "Reason: User Interface | Status: PLANNED")
+- [ ] [FFmpeg.wasm](https://github.com/ffmpegwasm/ffmpeg.wasm "Reason: Media Demuxing | Status: PLANNED")
---
+++ sw.js
@@ -0,0 +1,13 @@
+var DEFAULT_PROFILE={
+  version:"v0.0.0-ALPHA",
+  filesToCache:[
+    "/index.html",
+    "/script.js",
+    "/style.css",
+    "/src/coder.js",
+    "/src/debug.js",
+    "/src/ui.js",
+    "/src/jsondata/encformats.json",
+    "/src/libs/ffmpeg.min.js"
+  ]
+}
