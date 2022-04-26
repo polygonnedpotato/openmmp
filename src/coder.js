db9fca8ad26f378e747b38d7a652de9a69b851fe
elburg <whotookelburg@gmail.com>
2022-04-26 12:45:10 -0700

    doing smth

--- src/coder.js
+++ src/coder.js
@@ -2,11 +2,22 @@
 uses: encoding / decoding
 
 */
+const{createFFmpeg,fetchFile}=FFmpeg
+const FFmpeg=createFFmpeg({log:true})
+    
+// function determineDecoder(type=null){
+//   if(type=="mp3float"||type=="mp3"){
+//     return "FFMPEG"
+//   }
+// }
+function decodeMedia(inputData,inputDataType=null,options={}){
+  decoder="ffmpeg"
+  if(decoder=="ffmpeg"){
+    const{name}=inputData.files[0];
+    
+  }
+}
 const specs={
   decoding:{"ffmpeg":false,"libopenmpt":false,"vgmstream":false},
   encoding:{"ffmpeg":false}
 }
-addEventListener("message",event=>{
-  const[]
-})
-console.debug("coding worker ready")
